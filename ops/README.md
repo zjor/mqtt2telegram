971a190cb7b7528a9a3f383f324afdc6f5b69d5d
Sergey Royz <zjor.se@gmail.com>
2022-08-04 20:00:32 +0200

    secrets creation updated

--- ops/README.md
+++ ops/README.md
@@ -11,7 +11,8 @@ kubectl create secret generic environment \
   --from-literal=MQTT_PORT=${MQTT_PORT} \
   --from-literal=MQTT_USER=${MQTT_USER} \
   --from-literal=MQTT_PASSWORD=${MQTT_PASSWORD} \
-  --from-literal=TELEGRAM_TOKEN=${TELEGRAM_TOKEN} \      
+  --from-literal=TELEGRAM_USER_ID=${TELEGRAM_USER_ID} \  
+  --from-literal=TELEGRAM_TOKEN=${TELEGRAM_TOKEN} \
   --from-literal=TELEGRAM_BOT_USERNAME=${TELEGRAM_BOT_USERNAME} \
   --from-literal=API_BASE_URL=${API_BASE_URL} \
   -n app-mqtt2telegram
--- ops/create-secrets.sh
+++ ops/create-secrets.sh
@@ -2,12 +2,15 @@
 
 source ../.env
 
+kubectl delete secret environment -n app-mqtt2telegram
+
 kubectl create secret generic environment \
   --from-literal=MONGO_URI=${MONGO_URI} \
   --from-literal=MQTT_HOST=${MQTT_HOST} \
   --from-literal=MQTT_PORT=${MQTT_PORT} \
   --from-literal=MQTT_USER=${MQTT_USER} \
   --from-literal=MQTT_PASSWORD=${MQTT_PASSWORD} \
+  --from-literal=TELEGRAM_USER_ID=${TELEGRAM_USER_ID} \
   --from-literal=TELEGRAM_TOKEN=${TELEGRAM_TOKEN} \
   --from-literal=TELEGRAM_BOT_USERNAME=${TELEGRAM_BOT_USERNAME} \
   --from-literal=API_BASE_URL=${API_BASE_URL} \
